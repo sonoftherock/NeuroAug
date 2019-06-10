7e7eedfb66769b7a55a669a686e4977e7014848a
sonoftherock <trevorshim@outlook.kr>
2019-06-10 14:09:31 -0400

    clean up arguments

--- main.py
+++ main.py
@@ -16,24 +16,26 @@ from train import train_VAE, train_VGAE
 # Settings
 parser = argparse.ArgumentParser()
 parser.add_argument("data_dir", nargs='?', help="data directory", type=str, default="./data/BSNIP_left_full/original.npy")
-parser.add_argument("learning_rate", nargs='?', type=float, default=0.0001)
-parser.add_argument("epochs", nargs='?', type=int, default=1000000)
-parser.add_argument("batch_size", nargs='?', type=int, default=32)
 parser.add_argument("model_type", help='select augmentor model type. \
-                    Options: [VAE, VGAE, BrainNetCNN_VAE]', type=str)
-parser.add_argument("--hidden_dim_1", type=int, default=100)
-parser.add_argument("--hidden_dim_2", type=int, default=50)
-parser.add_argument("--hidden_dim_3", type=int, default=5)
-parser.add_argument("dropout", nargs='?', type=float, default=0.)
+                        Options: [VAE, VGAE, BrainNetCNN_VAE]', type=str)
+parser.add_argument("--hidden_dim_1", type=int, default=512)
+parser.add_argument("--hidden_dim_2", type=int, default=256)
+parser.add_argument("--hidden_dim_3", type=int, default=10)
+parser.add_argument("--batch_size", nargs='?', type=int, default=32)
+parser.add_argument("--epochs", nargs='?', type=int, default=1000000)
+parser.add_argument("--learning_rate", nargs='?', type=float, default=0.0001)
+parser.add_argument("--dropout", nargs='?', type=float, default=0.)
 parser.add_argument('--debug', help='turn on tf debugger', action="store_true")
 parser.add_argument('--restore', help='restore or train new model', action="store_true")
 parser.add_argument('--lambd', help='lagrange multiplier on constraint (MSE)', default=1.0)
 parser.add_argument('--alpha', help='slowness of the constraint ma', default=0.99)
+parser.add_argument('--tol', help='tolerance value for GECO training', default=0.03)
 
 args = parser.parse_args()
 print("Learning Rate: " + str(args.learning_rate))
 print("Hidden dimensions: " + str(args.hidden_dim_1), str(args.hidden_dim_2), str(args.hidden_dim_3))
 print("Augmentor model type: " + args.model_type)
+print("Taming VAE: Lambda=%f, Alpha=%f, Tolerane=%f" % (args.lambd, args.alpha, args.tol))
 
 def define_placeholders(args, data_shape):
 
